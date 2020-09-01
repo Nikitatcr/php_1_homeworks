d2b180883c07f12bcac5846b30076abda13b887a
Nikita TCR <nikitatcr@gmail.com>
2020-09-01 18:05:23 +0300

    New gallery

--- Lesson_4/Homework_4/hw_3_v_2/index.php
+++ Lesson_4/Homework_4/hw_3_v_2/index.php
@@ -1,1 +1,96 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Responsive Image Gallery</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="Responsive Image Gallery with jQuery" />
+    <meta name="keywords" content="jquery, carousel, image gallery, slider, responsive, flexible, fluid, resize, css3" />
+    <meta name="author" content="Codrops" />
+    <link rel="shortcut icon" href="../favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/demo.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/elastislide.css" />
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow&v1' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css' />
+    <noscript>
+        <style>
+            .es-carousel ul{
+                display:block;
+            }
+        </style>
+    </noscript>
+    <script id="img-wrapper-tmpl" type="text/x-jquery-tmpl">
+			<div class="rg-image-wrapper">
+				{{if itemsCount > 1}}
+					<div class="rg-image-nav">
+						<a href="#" class="rg-image-nav-prev">Previous Image</a>
+						<a href="#" class="rg-image-nav-next">Next Image</a>
+					</div>
+				{{/if}}
+				<div class="rg-image"></div>
+				<div class="rg-loading"></div>
+				<div class="rg-caption-wrapper">
+					<div class="rg-caption" style="display:none;">
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</script>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+
+
+        <div class="clr"></div>
+    </div><!-- header -->
+
+    <div class="content">
+        <h1>Альбом фотографий <span>Версия 1</span></h1>
+        <div id="rg-gallery" class="rg-gallery">
+            <div class="rg-thumbs">
+                <!-- Elastislide Carousel Thumbnail Viewer -->
+                <div class="es-carousel-wrapper">
+                    <div class="es-nav">
+                        <span class="es-nav-prev">Previous</span>
+                        <span class="es-nav-next">Next</span>
+                    </div>
+                    <div class="es-carousel">
+                        <ul>
+                            <?php
+
+                            function makeGallery($images) {
+                                $counter = 0;
+                                for($i=2;$i<count($images);$i++):
+                                    $info = new SplFileInfo($i-1);
+                                    $counter = ++ $counter;?>
+                                    <li>
+                                        <a href="#">
+                                        <img src="images/<?= $images[$i]?>" data-large="images/<?= $images[$i]?>" alt="image<?=$counter?>" data-description="<?=$info?>" />
+                                        </a>
+                                    </li>
+                                <?php
+                                endfor;
+                            }
+                            ?>
+
+                            <?php
+                            makeGallery(scandir("images/"));
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+                <!-- End Elastislide Carousel Thumbnail Viewer -->
+            </div><!-- rg-thumbs -->
+        </div><!-- rg-gallery -->
+    </div><!-- content -->
+</div><!-- container -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.tmpl.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.elastislide.js"></script>
+<script type="text/javascript" src="js/gallery.js"></script>
+</body>
+</html>
